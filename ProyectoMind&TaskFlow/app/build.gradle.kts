1a9c6bd54f06842a433543f254bf0a728e7cc41a
L0rdxavi97 <jaaca97@gmail.com>
2024-05-05 20:11:31 +0200

    add dependencies commit

--- ProyectoMind&TaskFlow/app/build.gradle.kts
+++ ProyectoMind&TaskFlow/app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.8.8")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
