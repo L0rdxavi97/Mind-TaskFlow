6baf63c8d6c68487482b63e5b046f6433d3023a8
L0rdxavi97 <jaaca97@gmail.com>
2024-05-21 23:33:02 +0200

    implement password hint commit

--- ProyectoMind&TaskFlow/app/src/main/java/com/example/proyectomindtaskflow/MainActivity.java
+++ ProyectoMind&TaskFlow/app/src/main/java/com/example/proyectomindtaskflow/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.proyectomindtaskflow;
 
+import android.app.sdksandbox.SandboxedSdk;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,10 +21,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "MyPrefsFile";
     public EditText usr;
     public EditText psw;
     public Button logbtn;
-    public Button crtbtn;
+    public Button crtbtn,hintbtn;
     public ManejadorBDTablas manejadorBDTablas ;
 
     @Override
@@ -32,11 +38,12 @@ public class MainActivity extends AppCompatActivity {
         psw=findViewById(R.id.ettPswd);
         logbtn=findViewById(R.id.logbtn);
         crtbtn=findViewById(R.id.btncrt);
+        hintbtn=findViewById(R.id.hintbtn);
 
         logbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(usr.getText().toString()!="" && psw.getText().toString()!="")
+                if(usr.getText().toString().isEmpty() || psw.getText().toString().isEmpty())
                 {
                     manejadorBDTablas.check_user(usr.getText().toString(), psw.getText().toString(), new CheckUserCallback() {
                     @Override
@@ -54,14 +61,6 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     Log.d("MainActivity", "Fallo en la autenticación del usuario.");
                 }
-                //manejadorBDTablas.check_user(usr.getText().toString(),psw.getText().toString());
-                //boolean a= ManejadorBDTablas.get_check_user();
-                System.out.println("log Usuario: "+usr.getText());
-                System.out.println("log passwd: "+psw.getText());
-                //System.out.println(a);
-//                if(a){
-//                    intento();
-//                }
 
             }
         });
@@ -69,9 +68,33 @@ public class MainActivity extends AppCompatActivity {
         crtbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Crear Usuario: "+usr.getText());
-                System.out.println("Crear passwd: "+psw.getText());
                 intento2();
+            }
+        });
+
+
+
+        hintbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(usr.getText().toString().isEmpty()){
+                    dialog("Nombre de usuario es requerido");
+                }else{
+                    manejadorBDTablas.check_userName(usr.getText().toString(), new CheckUserNameCallback() {
+                        @Override
+                        public void onCheckUserNameResult(boolean success) {
+                            if (success) {
+                                dialog(gettext(MainActivity.this,"hint",""));
+                            } else {
+                                dialog("Nombre de usuario incorrecto");
+                            }
+                        }
+                    });
+                }
+
+
             }
         });
 
@@ -95,5 +118,26 @@ public class MainActivity extends AppCompatActivity {
         // Verificar el estado de la autenticación
         boolean isAuthenticated = ManejadorBDTablas.get_check_user();
         Log.d("MainActivity", "Usuario autenticado: " + isAuthenticated);
+    }
+
+    public static String gettext(Context context, String key, String defaultValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(key, defaultValue);
+    }
+
+    public void dialog(String n){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Hint");
+
+        builder.setMessage(n);
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
