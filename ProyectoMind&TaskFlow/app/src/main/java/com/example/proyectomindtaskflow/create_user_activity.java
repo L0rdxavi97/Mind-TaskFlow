3aca103f9e30464735e952720a76aebc6999b64d
L0rdxavi97 <jaaca97@gmail.com>
2024-05-28 14:39:26 +0200

    implement pick image commit

--- ProyectoMind&TaskFlow/app/src/main/java/com/example/proyectomindtaskflow/create_user_activity.java
+++ ProyectoMind&TaskFlow/app/src/main/java/com/example/proyectomindtaskflow/create_user_activity.java
@@ -2,11 +2,13 @@ package com.example.proyectomindtaskflow;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,9 +27,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class create_user_activity extends AppCompatActivity {
-    private EditText nombre;
-    private EditText password;
-    private EditText frase;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    public EditText nombre;
+    public EditText password;
+    public EditText frase;
+    public ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,7 @@ public class create_user_activity extends AppCompatActivity {
         nombre = findViewById(R.id.ett_new_username);
         password = findViewById(R.id.ett_new_passwd);
         frase = findViewById(R.id.ett_new_r_hint);
+        imagen=findViewById(R.id.newuserimg);
         Button boton = findViewById(R.id.create_user_btn);
         Button botoncan = findViewById(R.id.btncancel);
         ManejadorBDTablas manejadorBDTablas= ManejadorBDTablas.getInstance(getApplicationContext());
@@ -74,11 +80,37 @@ public class create_user_activity extends AppCompatActivity {
                 intento();
             }
         });
+
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickImage();
+            }
+        });
     }
 
     public void intento() {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
+    }
+
+
+
+    private void pickImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            imagen.setImageURI(uri);
+        }
     }
 }
